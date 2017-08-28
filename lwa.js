f9a828d49437eb4191bcc868861e96678968064d
Sandbagger <wneal_19@yahoo.co.uk>
2017-08-28 17:30:06 +0100

    refactored code to remove obejcts that stored returned data

--- lwa.js
+++ lwa.js
@@ -1,26 +1,16 @@
-//Store geolocation playload
-var visitorLocation = {
-  longitude: 23,
-  latitude: 1};
-
- var weatherPayload = [];
 
 //Return visitor long and lat
 window.onload = function() {
-  var startPos;
+ 
   var geoOptions = {
      timeout: 10 * 1000
   }
 
   var geoSuccess = function(position) {
-    console.log(position);
-    startPos = position;
-    visitorLocation.longitude = startPos.coords.longitude;
-    visitorLocation.latitude = startPos.coords.latitude;
-    console.log("geosucc");
-    console.log(visitorLocation.longitude);
-    //call weather here because of execution context of this callback
-    callOpenWeather();
+   var long = position.coords.longitude;
+   var lat = position.coords.latitude;
+   //pass coordinates to weather API
+    callOpenWeather(long, lat);
   };
   var geoError = function(error) {
     console.log('Error occurred. Error code: ' + error.code);
@@ -30,19 +20,16 @@ window.onload = function() {
     //   2: position unavailable (error response from location provider)
     //   3: timed out
   };
-
+  //must go after callback definitions due to hoisting
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-  console.log("window onload");
   
 };
 
-console.log(visitorLocation.longitude);
-
-//Make weather api call console.log(visitorLocation.longitude);
-function callOpenWeather () {
+function callOpenWeather(longitude, latitude) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+visitorLocation.latitude+"&lon="+visitorLocation.longitude+"&APPID=a81ada84fca5c84e4168cd23c53c30f8", true);
-  xhr.send(null)
+  //modify URL with coordinates
+  xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=a81ada84fca5c84e4168cd23c53c30f8", true);
+  xhr.send(null);
   //Extract weather data from weather payload with icon
   xhr.onload = function () {
        if (xhr.status === 200) {
